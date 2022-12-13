34dcc0dc686acef56f1f28f890af84c295171ae4
rajan-zs <rajan.kumar@zopsmart.com>
2022-12-13 12:23:00 +0530

    increased coverage from 78% to 95%

--- examples/using-file/handler/handler_test.go
+++ examples/using-file/handler/handler_test.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"errors"
+	"io/fs"
+	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,23 +20,27 @@ func Test_Read(t *testing.T) {
 	app := gofr.NewWithConfig(&c)
 	ctx := gofr.NewContext(nil, nil, app)
 
-	l, err := file.NewWithConfig(app.Config, "../test.txt", "rw")
-	if err != nil {
-		t.Error("Unable to initialize", err)
-	}
-
-	f := New(l)
-
 	testcases := []struct {
-		desc   string
-		expErr error
-		resp   interface{}
-		ctx    *gofr.Context
+		desc     string
+		expErr   error
+		resp     interface{}
+		fileName string
+		fileMode string
+		ctx      *gofr.Context
 	}{
-		{"Successful Read", nil, "Welcome to Zopsmart!", ctx},
+		{"Successful Read", nil, "Welcome to Zopsmart!", "../test.txt", "rw", ctx},
+		{desc: "Invalid file", expErr: &fs.PathError{Op: "open", Path: "", Err: syscall.Errno(2)}, fileMode: "rw", ctx: ctx},
+		{"Failed to Read", errors.New("EOF"), nil, "../abc.txt", "rw", ctx},
 	}
 
 	for i, tc := range testcases {
+		l, err := file.NewWithConfig(app.Config, tc.fileName, file.Mode(tc.fileMode))
+		if err != nil {
+			t.Error("Unable to initialize", err)
+		}
+
+		f := New(l)
+
 		resp, err := f.Read(tc.ctx)
 		assert.Equalf(t, tc.resp, resp, "Test case %v failed.\nExpected: %v, got: %v", i, tc.resp, resp)
 
@@ -48,22 +55,25 @@ func Test_Write(t *testing.T) {
 	app := gofr.NewWithConfig(&c)
 	ctx := gofr.NewContext(nil, nil, app)
 
-	l, err := file.NewWithConfig(app.Config, "../test.txt", "rw")
-	if err != nil {
-		t.Error("Unable to initialize", err)
-	}
-
-	f := New(l)
-
 	testcases := []struct {
-		expErr error
-		resp   interface{}
-		ctx    *gofr.Context
+		expErr   error
+		resp     interface{}
+		ctx      *gofr.Context
+		fileName string
+		fileMode file.Mode
 	}{
-		{nil, "File written successfully!", ctx},
+		{nil, "File written successfully!", ctx, "../test.txt", "rw"},
+		{&fs.PathError{Op: "write", Path: "../test.txt", Err: syscall.Errno(9)}, nil, ctx, "../test.txt", "0444"},
+		{expErr: &fs.PathError{Op: "open", Path: "", Err: syscall.Errno(2)}, fileMode: "rw", ctx: ctx},
 	}
 
 	for i, tc := range testcases {
+		l, err := file.NewWithConfig(app.Config, tc.fileName, tc.fileMode)
+		if err != nil {
+			t.Error("Unable to initialize", err)
+		}
+
+		f := New(l)
 		resp, err := f.Write(tc.ctx)
 		assert.Equalf(t, tc.resp, resp, "Test case %v failed.\nExpected: %v, got: %v", i, tc.resp, resp)
 		assert.Equalf(t, tc.expErr, err, "Test case %v failed.\nExpected: %v, got: %v", i, tc.expErr, err)
